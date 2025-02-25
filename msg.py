1712b1ad81ea9cf76396f221a7eed2a491aa2859
cleanmind11 <kiranhuxley11@gmail.com>
2025-02-25 14:25:33 -0400

    change phone number

--- msg.py
+++ msg.py
@@ -1,6 +1,6 @@
 import http.client
 conn = http.client.HTTPSConnection("api.ultramsg.com")
-payload = "token=wl9xgibiztm6loex&to=380687400721&body=WhatsApp API on UltraMsg.com works good&priority=10&referenceId="
+payload = "token=wl9xgibiztm6loex&to=**********&body=WhatsApp API on UltraMsg.com works good&priority=10&referenceId="
 headers = { 'content-type': "application/x-www-form-urlencoded" }
 conn.request("POST", "/instance16/messages/chat", payload, headers)
 res = conn.getresponse()
